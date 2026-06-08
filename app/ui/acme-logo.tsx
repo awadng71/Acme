2d48fc7b81144c86d5ffd9d3bda45582fb5fc422
awadng <awadng2020@gmail.com>
2026-06-08 13:03:33 +0000

    first commit

---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,15 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { LusiT } from '@/app/ui/font';
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${LusiT.className} flex flex-row items-center leading-none text-white`}
+    >
+      <GlobeAltIcon 
+      className="h-12 w-12 rotate-[15deg]" 
+      style  ={{width: '48px', height: '48px'}}/>
+      <p className="text-[44px]-blue-500 ">Acme</p>
+    </div>
+  );
+}
